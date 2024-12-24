efe5f04c25c95ce4b51de354419f513e6f42220a
sirUnchained <bbehnam3831@gmail.com>
2024-12-24 18:12:45 +0330

    finished category in api get way

--- api_getway/src/services/category.service.ts
+++ api_getway/src/services/category.service.ts
@@ -26,7 +26,15 @@ export const getSingle = async (
   reply: FastifyReply
 ): Promise<void> => {
   try {
-    // todo
+    const apiRegister = await getService("categories");
+    const { categoryID } = request.params as { categoryID: string };
+    const result = await breaker.callService({
+      method: "GET",
+      url: `http://localhost:${apiRegister[0].port}/categories/${categoryID}`,
+    });
+
+    reply.status(result.status).send(result.data);
+    return;
   } catch (error) {
     throw error;
   }
@@ -37,7 +45,18 @@ export const remove = async (
   reply: FastifyReply
 ): Promise<void> => {
   try {
-    // todo
+    const apiRegister = await getService("categories");
+    const { categoryID } = request.params as { categoryID: string };
+    const result = await breaker.callService({
+      method: "DELETE",
+      url: `http://localhost:${apiRegister[0].port}/categories/${categoryID}`,
+      headers: {
+        Authorization: `Bearer ${request.token}`,
+      },
+    });
+
+    reply.status(result.status).send(result.data);
+    return;
   } catch (error) {
     throw error;
   }
@@ -48,7 +67,19 @@ export const create = async (
   reply: FastifyReply
 ): Promise<void> => {
   try {
-    // todo
+    const apiRegister = await getService("categories");
+    const { name } = request.body as { name: string };
+    const result = await breaker.callService({
+      method: "POST",
+      url: `http://localhost:${apiRegister[0].port}/categories`,
+      headers: {
+        Authorization: `Bearer ${request.token}`,
+      },
+      data: { name },
+    });
+
+    reply.status(result.status).send(result.data);
+    return;
   } catch (error) {
     throw error;
   }
